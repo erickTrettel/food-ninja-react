8c0ec4ade9e16ca121324d14dbc8c5315555690a
Erick Trettel <ericktrettel@gmail.com>
2019-09-17 16:54:23 -0300

    Criando pagina de login

--- src/components/home/login.jsx
+++ src/components/home/login.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login-box">
+      <p className="login-title">Login</p>
+
+      <div className="login-form">
+        <div className="form-group">
+          <label htmlFor="email">E-mail</label>
+          <input type="email" id="email" name="email" className="form-control"/>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" className="form-control"/>
+        </div>
+
+        <button type="button" className="btn btn-primary btn-block">
+          Login
+        </button>
+      </div>
     </div>
   )
 }
--- src/components/template/navbar.jsx
+++ src/components/template/navbar.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 function Navbar() {
   return (
     <div>
-      <ul class="nav bg-dark justify-content-end">
-        <li class="nav-item">
-          <a class="nav-link text-white" href="/home">Home</a>
+      <ul className="nav bg-dark justify-content-end">
+        <li className="nav-item">
+          <a className="nav-link text-white" href="/home">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="/dashboard">Dashboard</a>
+        <li className="nav-item">
+          <a className="nav-link text-white" href="/dashboard">Dashboard</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link text-white-50" href="/login">Login</a>
+        <li className="nav-item">
+          <a className="nav-link text-white-50" href="/login">Login</a>
         </li>
       </ul>
     </div>
